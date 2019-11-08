fc52bb72fc33cb76dba994142a6f1f3b322764f5
Antony Omeri <antony.omeri@dtone.com>
2019-11-09 00:53:31 +0300

    Initial commit

---
+++ src/test/java/org/mifos/ussd/MifosUssdApplicationTests.java
@@ -0,0 +1,13 @@
+package org.mifos.ussd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MifosUssdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
