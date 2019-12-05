d6ff17a3b1a1a56374337b5ae127134c92551343
Antony Omeri <antony.omeri@dtone.com>
2019-12-05 16:29:04 +0300

    Upadte age method to return seconds

--- src/main/java/org/mifos/ussd/utils/DateUtil.java
+++ src/main/java/org/mifos/ussd/utils/DateUtil.java
@@ -2,7 +2,7 @@ package org.mifos.ussd.utils;
 
 
 import org.joda.time.LocalDateTime;
-import org.joda.time.Minutes;
+import org.joda.time.Seconds;
 
 import java.util.Date;
 
@@ -11,13 +11,13 @@ import java.util.Date;
  */
 public class DateUtil {
 
-    public static int getAge(Date date) {
-        int age = 0;
+    public static int getAgeSeconds(Date date) {
+        int age;
         LocalDateTime localDate = new LocalDateTime(date);
         LocalDateTime now = LocalDateTime.now();
 
-        Minutes minutes = Minutes.minutesBetween(localDate, now);
-        age = minutes.getMinutes();
+        Seconds seconds = Seconds.secondsBetween(localDate, now);
+        age = seconds.getSeconds();
 
         return age;
     }
